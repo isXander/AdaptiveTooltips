ba9878f692bbe35a6cbb49d0a46196c1cfbc9880
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2026-05-26 15:04:29 +0800

    Fix transparency

--- src/main/java/dev/isxander/adaptivetooltips/mixins/TooltipRenderUtilMixin.java
+++ src/main/java/dev/isxander/adaptivetooltips/mixins/TooltipRenderUtilMixin.java
@@ -2,23 +2,21 @@ package dev.isxander.adaptivetooltips.mixins;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 import dev.isxander.adaptivetooltips.config.AdaptiveTooltipConfig;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.TooltipRenderUtil;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.function.Function;
 
 @Mixin(TooltipRenderUtil.class)
 public class TooltipRenderUtilMixin {
     @WrapOperation(
             method = "renderTooltipBackground",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Ljava/util/function/Function;Lnet/minecraft/resources/ResourceLocation;IIII)V")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIII)V")
     )
-    private static void changeBackgroundColor(GuiGraphics instance, Function<ResourceLocation, RenderType> function, ResourceLocation id, int i, int j, int k, int l, Operation<Void> original) {
-        instance.blitSprite(function, id, i, j, k, l, (int) (AdaptiveTooltipConfig.HANDLER.instance().tooltipTransparency * 255f) << 24 | 0xFFFFFF);
+    private static void changeBackgroundColor(GuiGraphics instance, RenderPipeline renderPipeline, ResourceLocation id, int i, int j, int k, int l, Operation<Void> original) {
+        instance.blitSprite(renderPipeline, id, i, j, k, l, AdaptiveTooltipConfig.HANDLER.instance().tooltipTransparency);
     }
 }
