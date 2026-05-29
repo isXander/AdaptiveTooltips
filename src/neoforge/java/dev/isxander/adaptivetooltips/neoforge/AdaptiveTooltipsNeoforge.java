381202514e3274f3e7ab387f9fc53c727830dd9c
isXander <xander@isxander.dev>
2026-05-29 18:08:01 +0100

    client-only on NeoForge

--- src/neoforge/java/dev/isxander/adaptivetooltips/neoforge/AdaptiveTooltipsNeoforge.java
+++ src/neoforge/java/dev/isxander/adaptivetooltips/neoforge/AdaptiveTooltipsNeoforge.java
@@ -3,11 +3,12 @@ package dev.isxander.adaptivetooltips.neoforge;
 import dev.isxander.adaptivetooltips.AdaptiveTooltips;
 import dev.isxander.adaptivetooltips.config.AdaptiveTooltipConfig;
 import dev.isxander.adaptivetooltips.platform.AdaptiveTooltipsPlatform;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod("adaptive_tooltips")
+@Mod(value = "adaptive_tooltips", dist = Dist.CLIENT)
 public class AdaptiveTooltipsNeoforge {
     public AdaptiveTooltipsNeoforge() {
         AdaptiveTooltipsPlatform.ImplHolder.IMPL = new AdaptiveTooltipsPlatformNeoforge();
